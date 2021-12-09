44b6a7f520919a25a8797c71ea1b5f66d98aa79b
Soumalya Paul <samratghosh240@gmail.com>
2021-12-10 00:59:09 +0530

    intial commit

---
+++ addoftwonumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number\n");
+    scanf("%d",&n);
+    int t;
+    int m=n-1;
+    for(t=1;t<=m;t++)
+    {
+        if((t+m==n)&&(t!=m))
+        printf("%d + %d\n ",t,m);   
+        m--;
+ }
+}
